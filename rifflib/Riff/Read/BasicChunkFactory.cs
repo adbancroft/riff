ef33f3747bbee15c668acd6c9f8fdcf0ca1562a6
adbancroft <adbancroft@users.noreply.github.com>
2020-11-02 11:18:54 -0600

    Fix XML comments

--- rifflib/Riff/Read/BasicChunkFactory.cs
+++ rifflib/Riff/Read/BasicChunkFactory.cs
@@ -4,12 +4,14 @@ using Riff.Read.Chunk;
 namespace Riff.Read
 {
     /// <summary>
-    /// A <see cref="IChunkFactory"/> that works with the simplest chunk types.
+    /// A <see cref="IChunkFactory"/> that works with the simplest chunk types 
+    /// and initializes chunks during construction by reading from a binary source.
     /// </summary>
     public class BasicChunkFactory : IChunkFactory
     {
         private readonly BinaryReader _reader;
 
+        /// <param name="reader">Binary data source to initialize chunks from</param>
         public BasicChunkFactory(BinaryReader reader)
         {
             _reader = reader;
